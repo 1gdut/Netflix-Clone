c3bd9092b2a696327ca88d93ce94d53b1c1cb78e
xiaoruitao <1226324785@qq.com>
2025-10-07 23:28:47 +0800

    init

---
+++ Netflix-Clone/Netflix-Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by xrt on 2025/10/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
