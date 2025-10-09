0687898d0e7f0b76b7fd6d4a41cd717fc0e40b42
xiaoruitao <1226324785@qq.com>
2025-10-09 14:52:22 +0800

    refactor: 重构主界面并统一视图背景色
    - 删除旧的 ViewController 并创建新的 MainTabBarViewController
    - 为所有子视图控制器设置统一的 systemBackground 背景色
    - 更新 SceneDelegate 使用新的主控制器

---
+++ Netflix-Clone/Netflix-Clone/MainTabBarViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Netflix-Clone
+//
+//  Created by xrt on 2025/10/7.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemYellow
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: UpComingViewController())
+        let vc3 = UINavigationController(rootViewController: SearchViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "play.circle")
+        vc3.tabBarItem.image = UIImage(systemName: "magnifyingglass")
+        vc4.tabBarItem.image = UIImage(systemName: "arrow.down.to.line")
+        vc1.title = "主页"
+        vc2.title = "即将上映"
+        vc3.title = "搜索"
+        vc4.title = "下载"
+        tabBar.tintColor = .label
+
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
+        
+    }
+
+
+}
+
