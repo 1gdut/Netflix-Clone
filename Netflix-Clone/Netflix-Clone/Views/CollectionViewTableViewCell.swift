dd8a956522b360af2c4c3cb0944a432cdf6955d7
xiaoruitao <1226324785@qq.com>
2025-10-09 17:00:28 +0800

    feat(视图): 添加CollectionViewTableViewCell基础实现

---
+++ Netflix-Clone/Netflix-Clone/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix-Clone
+//
+//  Created by xrt on 2025/10/9.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
