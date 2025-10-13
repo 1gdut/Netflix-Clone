70b6810af2f333d50823cda65f6954426a59d76a
xiaoruitao <1226324785@qq.com>
2025-10-13 16:18:41 +0800

    feat(API): 实现获取 trending movies 的 API 功能
    添加 Movie 数据模型和 APIError 枚举
    重构 APICaller 中的 getTrendingMovies 方法，使用 Result 类型处理返回结果
    在 HomeViewController 中处理 API 返回数据

--- Netflix-Clone/Netflix-Clone/Managers/APICaller.swift
+++ Netflix-Clone/Netflix-Clone/Managers/APICaller.swift
@@ -12,23 +12,26 @@ struct Constants {
     static let baseURL = "https://api.themoviedb.org/3"
 }
 
+enum APIError: Error {
+    case failedToGetData
+}
+
 class APICaller {
     static let shared = APICaller()
 
-    func getTrendingMovies(completion: @escaping (String) -> Void) {
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
         guard let url = URL(string: "\(Constants.baseURL)/trending/all/day?api_key=\(Constants.API_KEY)") else {
             return
         }
-        print(url)
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
             guard let data = data, error == nil else {
                 return
             }
             do {
-                let response = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
-                print(response)
+                let results = try JSONDecoder().decode(TrendingMoviesResponse.self, from: data)
+                completion(.success(results.results))
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(error))
             }
         }
         task.resume()
