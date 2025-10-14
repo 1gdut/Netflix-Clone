81eefd9583ec8d8c0087abe276835d75d6b3293a
xiaoruitao <1226324785@qq.com>
2025-10-14 16:17:20 +0800

    feat(UI): 添加标题集合视图单元格基础实现
    实现 Netflix-Clone 应用中的标题集合视图单元格，包含海报图片视图的初始布局和自动调整大小功能

---
+++ Netflix-Clone/Netflix-Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix-Clone
+//
+//  Created by xrt on 2025/10/14.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+}
